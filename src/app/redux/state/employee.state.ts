e60816fc092c71f397c127c3b0531bf8a74408df
Theresa Jv Rensburg <theresajvrensburg@Theresas-MacBook-Air.local>
2021-06-10 14:16:05 +0200

    Change redux store to initialise data on app loading

--- src/app/redux/state/employee.state.ts
+++ src/app/redux/state/employee.state.ts
@@ -1,7 +1,7 @@
 import { State, Action, StateContext, Selector } from '@ngxs/store';
 import { Employee } from '../models/employee';
 import { AddEmployee } from '../actions/employee.action';
-import {Observable} from "rxjs";
+import { Observable } from "rxjs";
 
 export class EmployeeStateModel {
   employees: Employee[] = [];
